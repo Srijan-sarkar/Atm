b12aeba2195703a1eeb79e811f52efa52ec2cf90
Srijan-sarkar <68334108+Srijan-sarkar@users.noreply.github.com>
2021-07-11 20:53:12 +0530

    Add files via upload

--- atm.py
+++ atm.py
@@ -1,30 +1,26 @@
-Card_no = input("Enter your card number") 
-Password = input("Enter the password")
-Withdraw_or_Balance = input("Would you like to withdraw or check your balance")
-if Withdraw_or_Balance==1:
-                print("Your balance is 50000")
-elif Withdraw_or_Balance==2:
-                Withdraw = input("Enter the number you need")
-                Balance = 50000-Withdraw
-                print("You have successfully Withdrawn ",Withdraw)
-                print("You have ",Balance," left")
-else:
-                print("Enter a valid number") 
-
 class Atm():
+    def __init__(self,choice):
+        self.choice = choice
     
-    
-    def start():
-            if Withdraw_or_Balance==1:
+    def start(self,choice):
+            if (choice==1):
                 print("Your balance is 50000")
-            elif Withdraw_or_Balance==2:
-                Withdraw = input("Enter the number you need")
+            elif (choice == 2):
+                Withdraw = int(input("Enter the number you need"))
                 Balance = 50000-Withdraw
                 print("You have successfully Withdrawn ",Withdraw)
                 print("You have ",Balance," left")
             else:
                 print("Enter a valid number")        
 
-Attempt = Atm()
 
-Attempt.start
+
+def main():
+    Card_no = input("Enter your card number") 
+    Password = input("Enter the password")
+    Withdraw_or_Balance = int(input("Press 1 if you would you like to withdraw or Press 2 if you want to check your balance"))
+    
+    Attempt = Atm(Withdraw_or_Balance)
+    Attempt.start(Withdraw_or_Balance)
+
+main()
